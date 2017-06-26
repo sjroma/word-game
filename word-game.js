6e165f8c7504201c5b0ca2738c28edb65fe5772f
Steve Roma <sjroma@hotmail.com>
2017-06-26 16:44:03 -0400

    installed packages, update and index.mustache displays when port is first hit

--- word-game.js
+++ word-game.js
@@ -1,16 +1,18 @@
-var express = require('express')
-var parseurl = require('parseurl')
-var session = require('express-session')
+const express = require('express');
+const parseurl = require('parseurl');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mustacheExpress = require('mustache-express');
+const app = express();
 var fs = require('fs');
-
-
-var app = express()
 
 
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 //console.log(words);  // Gets a crap-ton of words
 
+app.engine('mustache', mustacheExpress());
 app.set('views', './views');
+app.set('view engine', 'mustache');
 
 app.use(session({
   secret: 'keyboard cat',
@@ -36,6 +38,11 @@ app.use(function (req, res, next) {
   next()
 })
 
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+
 app.get('/foo', function (req, res, next) {
   res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
 })
@@ -45,5 +52,5 @@ app.get('/bar', function (req, res, next) {
 })
 
 app.listen(3000, function () {
-  console.log('Started word-game application')
+  console.log('word-game application listening on port 3000')
 });
