dc6a962fa65b6f9223799cf029c22d168c229de0
Steve Roma <sjroma@hotmail.com>
2017-07-11 11:34:30 -0400

    rename gameplay.js to avoid confusion with vew, add get and post for when game is in progress

--- word-game.js
+++ word-game.js
@@ -5,29 +5,22 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const mustacheExpress = require('mustache-express');
 
-//calling objects from gamplay.js
-const gameplay = require('./gameplay')
-
-var fs = require('fs'); // File System module (part of Node.js)
-
-// goes to the computer dictionary and gets a random word
-const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
-//console.log(words);  // Gets a crap-ton of words
+// calling objects from gamedata.js
+const gamedata = require('./gamedata')
 
 var guessedLetter = '';  // this will be the letter the user guesses
 const rememGuess = [];  // this will be all the letters the user guessed
 var guessLeft = 8;
 
 // Word Game voodoo
-var randomWord = words[Math.floor(Math.random() * words.length)];
-//let randomWord = gameplay.ranWord; //doesn't work
+let randomWord = gamedata.randomWord;
 var secretWord = randomWord;
 console.log("in declarations the secWord is:", randomWord);
 var swArray = randomWord.split(""); // puts secret word in an array, letters separated by commas
-console.log("secWord in array is:", swArray);
+console.log("in declarations secWord in array is:", swArray);
 
-let displaySW = gameplay.makeD(swArray);
-let joinSW = gameplay.makeD(swArray);
+let displaySW = gamedata.makeD(swArray);
+let joinSW = gamedata.makeD(swArray);
 let displayableSW = joinSW.join(''); // display dashes on screen the length of the word
 console.log("displaySW;", displaySW);
 console.log('displayableSW;', displayableSW);
@@ -60,21 +53,21 @@ app.use(session({
 // Set Static Path (this is where my static resourses, like CSS files, images, etc reside)
 app.use(express.static('public'));
 
-app.use(function (req, res, next) {
-	var views = req.session.views
-
-	if (!views) {
-		views = req.session.views = {}
-	}
-
-	// get the url pathname
-	var pathname = parseurl(req).pathname
-
-	// count the views
-	views[pathname] = (views[pathname] || 0) + 1
-
-	next()
-})
+//app.use(function (req, res, next) {
+//	var views = req.session.views
+//
+//	if (!views) {
+//		views = req.session.views = {}
+//	}
+//
+//	// get the url pathname
+//	var pathname = parseurl(req).pathname
+//
+//	// count the views
+//	views[pathname] = (views[pathname] || 0) + 1
+//
+//	next()
+//})
 
 // this should be the start of the game...
 app.get('/', function (req, res) {
@@ -88,21 +81,61 @@ app.get('/', function (req, res) {
 
 // this allows the form and console to talk, when this hits I still want the mystery word to show
 // this is the game in progress
-app.post('/', function(req, res, next){
-	let guess = req.body.yourGuess.toLowerCase();
-	req.checkBody('guessInput', "Please enter one letter").isLength({min:1, max:1}).isAlpha().notEmpty();
-	let guessErr = gameplay.isNewLetter(guess, guessedLetter)
-//	var errors = req.validationErrors();
-//	if (errors) {
-//		res.redirect('/');
-//	}
+app.post('/', function(req, res) {
+	guessedLetter = req.body.yourGuess.toLowerCase();
+	req.checkBody('yourGuess', "Please enter one letter").notEmpty().isLength({min:0, max:1}).isAlpha();
+	
+	var errors = req.validationErrors();
+	console.log("in app.post, guessInput:", guessedLetter);
+  if (errors) {
+    // Render validation error messages
+    res.render('gameplay', {errors: errors});
+		console.log("errors", errors);
+  } else {
+		let guess = gamedata.isNewLetter(guessedLetter, guessedLetter)
 	res.render('gameplay', {
 		word: displayableSW, guessedLetters: guessedLetter, remaining: guessLeft
-	});
+	  });
 	console.log("word-game app.post:", secretWord);
-	console.log("guessedLetter", guess);
+	console.log("app.post, guessedLetter", guessedLetter);
+  }
 });
 
+app.get('gameplay', function(req,res) {
+	guessedLetter = req.body.yourGuess.toLowerCase();
+	req.checkBody('yourGuess', "Please enter one letter").notEmpty().isLength({min:0, max:1}).isAlpha();
+	
+	var errors = req.validationErrors();
+	console.log("guessInput(gameplay.get):", guessedLetter);
+  if (errors) {
+    // Render validation error messages
+    res.render('gameplay', {errors: errors});
+		console.log("errors", errors);
+  } else {
+		let guess = gameplay.isNewLetter(guessedLetter, guessedLetter)
+	res.render('gameplay', {
+		word: displayableSW, guessedLetters: guessedLetter, remaining: guessLeft
+    });
+  }
+});
+		
+app.post('gameplay', function(req,res) {
+	guessedLetter = req.body.yourGuess.toLowerCase();
+	req.checkBody('yourGuess', "Please enter one letter").notEmpty().isLength({min:0, max:1}).isAlpha();
+	
+	var errors = req.validationErrors();
+	console.log("guessInput(gameplay.post):", guessedLetter);
+  if (errors) {
+    // Render validation error messages
+    res.render('gameplay', {errors: errors});
+		console.log("errors", errors);
+  } else {
+		let guess = gameplay.isNewLetter(guessedLetter, guessedLetter)
+	res.render('gameplay', {
+		word: displayableSW, guessedLetters: guessedLetter, remaining: guessLeft
+    });
+	}
+});
 
 //app.get('/foo', function (req, res, next) {
 //	res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
@@ -115,14 +148,3 @@ app.post('/', function(req, res, next){
 app.listen(3000, function () {
 	console.log('word-game application listening on port 3000')
 });
-
-// these semi-work...gonna copy and make changes, saving in case
-//app.post('/', function(req, res){
-//	var inputItem = req.body.yourGuess;
-//	res.render('gameplay', {
-//		word: displayableSW, guessedLetters: guessedLetter, remaining: guessLeft
-//	});
-//	console.log("in app post:", secretWord)
-//	console.log("guessedLetter", guessedLetter);
-//});
-// end saved, semi-work
