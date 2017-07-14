49096d08d1ed2366c9518a68d2b194266cf24d73
Steve Roma <sjroma@hotmail.com>
2017-07-13 23:00:47 -0400

    detects letters but no replace yet, decrements correctly, minor style changes

--- gamedata.js
+++ gamedata.js
@@ -32,6 +32,39 @@ function isNew(letter, string) {
 	return repeatGuess;
 }
 
+
+function checkLetter(req, string, wordArray){
+  let chosenLetter = string;
+  let count = 0;
+  if(chosenLetter.length === 1){
+    let letterCheck = wordArray.stringArr.find(function(lett){
+      if(lett.letter === chosenLetter){
+        lett.letterGuess = chosenLetter;
+        req.session.failed = false;
+      }
+      else{
+        count = count + 1;
+        // console.log("This is the count insid the else block: " + count);
+      }
+    });
+    // console.log("The count going into the bad letter array: " + count);
+    if(count === wordArray.stringArr.length){
+      req.session.failed = true;
+    }
+  }
+  let countCorrectLetters = 0;
+  for(let i = 0; i < wordArray.stringArr.length;i++){
+    // console.log(wordArray.stringArr.length);
+    if(wordArray.stringArr[i].letter === wordArray.stringArr[i].letterGuess){
+      countCorrectLetters = countCorrectLetters + 1;
+    }
+  }
+  if(countCorrectLetters === wordArray.stringArr.length){
+    req.session.youWon = true;
+  }
+  // console.log("The count is: " +countCorrectLetters);
+  return req.session
+}
 function checkGuess(req, string, wordArray) {
 	
 }
--- word-game.js
+++ word-game.js
@@ -14,16 +14,19 @@ var guessLeft = 8;
 // Word Game voodoo
 let randomWord = gamedata.randomWord;
 var secretWord = randomWord;
-console.log("voodoo.randomWord:", randomWord);
+console.log("voodoo.randomWord:", secretWord);
 var swArray = randomWord.split(""); // puts secret word in an array, letters separated by commas
 //console.log("voodoo.swArray:", swArray);
 let displaySW = gamedata.makeD(swArray);
 let joinSW = gamedata.makeD(swArray);
 let displayableSW = joinSW.join(''); // display dashes on screen the length of the word
+console.log("voodoo.secretWord:", secretWord);
 console.log("voodoo.displayableSW:", displayableSW);
 // end voodoo
 
 const app = express();
+// Set Static Path (this is where my static resourses, like CSS files, images, etc reside)
+app.use(express.static('public'));
 
 // Set app to use Body Parser middleware
 app.use(bodyParser.json());
@@ -33,7 +36,7 @@ app.use(bodyParser.urlencoded({
 //'extended: false' parses strings and arrays.
 //'extended: true' parses nested objects
 
-//'expressValidator' must come after 'bodyParser', since data must be parsed first!
+//'expressValidator' must come after 'bodyParser' since data must be parsed first
 app.use(expressValidator());
 
 // View Engine
@@ -41,14 +44,11 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
-app.use(session({
-	secret: 'puckHead cat',
-	resave: false,
-	saveUninitialized: true
-}))
-
-// Set Static Path (this is where my static resourses, like CSS files, images, etc reside)
-app.use(express.static('public'));
+//app.use(session({
+//	secret: 'puckHead cat',
+//	resave: false,
+//	saveUninitialized: true
+//}))
 
 // Session Authentication
 //app.use(function (req, res, next) {
@@ -73,6 +73,7 @@ app.get('/', function (req, res) {
 	//	incorrGuesses = '';
 	res.render('index', {
 		title: title,
+//		secWord: secretWord,
 		word: displayableSW,
 		remaining: guessLeft
 	});
@@ -82,15 +83,12 @@ app.get('/', function (req, res) {
 // this allows the form and console to talk
 app.post('/', function (req, res) {
 	guessedLetter = req.body.yourGuess.toLowerCase();
-	//	rememGuess.push(guessedLetter.toLowerCase());
-	//	console.log("app.post.rememGuess:", rememGuess);
 	req.checkBody('yourGuess', "Please enter one letter").notEmpty().isLength({
 		min: 0,
 		max: 1
 	}).isAlpha();
 
 	var errors = req.validationErrors();
-	//	console.log("app.post.guessedLetter:", guessedLetter);
 	if (errors) {
 		// Render validation error messages
 		res.render('gameplay', {
@@ -101,12 +99,27 @@ app.post('/', function (req, res) {
 		});
 		console.log("app.post.errors:", errors);
 	} else {
-		rememGuess.push(guessedLetter.toLowerCase());
+		rememGuess.push(guessedLetter.toLowerCase()); // push guesses to array and display them
 		let guess = gamedata.isNewLetter(guessedLetter, guessedLetter)
-//		console.log("app.post.guess:", guess);
-		console.log("app.post.guessedLetter:", guessedLetter);
 		console.log("app.post.rememGuess:", rememGuess);
+
+		
+		if (secretWord.includes(guessedLetter)) {
+			for (i = 0; i < secretWord.length; i++) {
+				if (secretWord[i] === guessedLetter) {
+					secretWord[i] = displayableSW[i].replace('_', guessedLetter);
+					console.log("app.post.guessedLetter: ", guessedLetter);
+				}
+			}
+		}
+		else {
+			guessLeft -= 1;
+		};;
+		
+
 		res.render('gameplay', {
+			title: title,
+//			secWord: secretWord,
 			word: displayableSW,
 			guessedLetters: rememGuess,
 			remaining: guessLeft
